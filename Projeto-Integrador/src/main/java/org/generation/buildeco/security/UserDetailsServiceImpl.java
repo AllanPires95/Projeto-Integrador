e4da6caa4a3566d318dcaefb309f8f653cbb67eb
MBeatrizMotta <mariabeatrizmottapino@gmail.com>
2023-02-08 10:09:49 -0300

    Bia

---
+++ Projeto-Integrador/src/main/java/org/generation/buildeco/security/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package org.generation.buildeco.security;
+
+import java.util.Optional;
+
+import org.generation.buildeco.model.UsuarioModel;
+import org.generation.buildeco.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.server.ResponseStatusException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+
+		Optional<UsuarioModel> usuario = usuarioRepository.findByUsuario(userName);
+
+		if (usuario.isPresent())
+			return new UserDetailsImpl(usuario.get());
+		else
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+	}
+}
+
+
